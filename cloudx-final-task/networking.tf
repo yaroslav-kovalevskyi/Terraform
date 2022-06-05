52118adf27f087f38e87a6c27fe32e04e396aea7
yaroslav_kovalevskyi <kovalevskyi96@gmail.com>
2022-06-05 14:48:11 +0300

    Network Stack - added 3 public subnets and associated them to IGW - added 3 private subnets, 3 db subnets and their traffic routed between themselves

---
+++ cloudx-final-task/networking.tf
@@ -0,0 +1,93 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = var.project
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.subnet_zones)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnet_cidr, count.index)
+  availability_zone = element(var.subnet_zones, count.index)
+
+  tags = {
+    Name = "public_${element(var.subnet_zones, count.index)}"
+    Tier = "Public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.subnet_zones)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnet_cidr, count.index)
+  availability_zone = element(var.subnet_zones, count.index)
+
+  tags = {
+    Name = "private_${element(var.subnet_zones, count.index)}"
+    Tier = "Private"
+  }
+}
+
+resource "aws_subnet" "db" {
+  count             = length(var.subnet_zones)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.db_subnet_cidr, count.index)
+  availability_zone = element(var.subnet_zones, count.index)
+
+  tags = {
+    Name = "private_db_${element(var.subnet_zones, count.index)}"
+    Tier = "Private"
+  }
+}
+
+resource "aws_internet_gateway" "to_Internet" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project}-igw"
+  }
+}
+
+resource "aws_route_table" "to_Internet" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.to_Internet.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+resource "aws_route_table_association" "public-internet" {
+  count          = length(var.subnet_zones)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.to_Internet.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private_rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.subnet_zones)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private-db" {
+  count          = length(var.subnet_zones)
+  subnet_id      = element(aws_subnet.db.*.id, count.index)
+  route_table_id = aws_route_table.private_rt.id
+}
+
--- cloudx-final-task/variables.tf
+++ cloudx-final-task/variables.tf
@@ -5,7 +5,7 @@
 variable "project" {
   type        = string
   description = "name of the project"
-  default     = "cloudx-yaroslav-k"
+  default     = "cloudx"
 }
 
 variable "region" {
@@ -14,3 +14,30 @@ variable "region" {
   default     = "eu-south-1"
 }
 
+/*--------------------
+------NETWORKING------
+--------------------*/
+
+variable "public_subnet_cidr" {
+  type        = list(any)
+  description = "List of subnets CIDRs"
+  default     = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+}
+
+variable "private_subnet_cidr" {
+  type        = list(any)
+  description = "List of subnets CIDRs"
+  default     = ["10.10.10.0/24", "10.10.11.0/24", "10.10.12.0/24"]
+}
+
+variable "db_subnet_cidr" {
+  type        = list(any)
+  description = "List of subnets CIDRs"
+  default     = ["10.10.20.0/24", "10.10.21.0/24", "10.10.22.0/24"]
+}
+#Attention: subnets availability zones are hardcoded and attached to the eu-south-1 region.
+variable "subnet_zones" {
+  type        = list(any)
+  description = "List of subnets AZs"
+  default     = ["eu-south-1a", "eu-south-1b", "eu-south-1c"]
+}
