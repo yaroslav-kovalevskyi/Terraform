137aa44ec6a4f6952ef56d3247933d8ce5a5103f
yaroslav_kovalevskyi <kovalevskyi96@gmail.com>
2022-06-05 23:32:23 +0300

    IAM Role - added IAM role provides EC2 and Fargate instances with access to the services (ec2, fargate)

---
+++ cloudx-final-task/iam.tf
@@ -0,0 +1,50 @@
+resource "aws_iam_role_policy" "ghost_app" {
+  name = "ghost_app_policy"
+  role = aws_iam_role.ghost_app.id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "kms:Decrypt",
+          "ssm:GetParameterHistory",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetAuthorizationToken",
+          "elasticfilesystem:ClientWrite",
+          "ssm:GetParameters",
+          "ssm:GetParameter",
+          "logs:PutLogEvents",
+          "ec2:Describe*",
+          "logs:CreateLogStream",
+          "secretsmanager:GetSecretValue",
+          "elasticfilesystem:ClientMount",
+          "ecr:BatchGetImage",
+          "ssm:GetParametersByPath",
+          "elasticfilesystem:DescribeFileSystems",
+          "ecr:BatchCheckLayerAvailability"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "ghost_app" {
+  name = "ghost_app_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = ["ec2.amazonaws.com", "ecs-tasks.amazonaws.com"]
+        }
+      },
+    ]
+  })
+}
