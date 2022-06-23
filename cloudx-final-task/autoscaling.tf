4c491e4453781ed11ce9710d74c2a076429cfd96
Yaroslav_Kovalevskyi <A3EFr4jNrBSuMCj>
2022-06-23 17:18:34 +0300

    Autoscaling update - added autoscaling group that will launch instance from recently created launch template

--- cloudx-final-task/autoscaling.tf
+++ cloudx-final-task/autoscaling.tf
@@ -6,15 +6,19 @@ resource "aws_launch_template" "ghost-lt" {
   user_data              = filebase64("ghost.sh")
   image_id               = data.aws_ami.latest_amazon_linux.image_id
   update_default_version = true
-  network_interfaces {
-    subnet_id       = aws_subnet.public[0].id
-    security_groups = [aws_security_group.ec2.id]
-  }
-  network_interfaces {
-    subnet_id       = aws_subnet.public[1].id
-    security_groups = [aws_security_group.ec2.id]
-  }
   iam_instance_profile {
     name = aws_iam_instance_profile.cloudx_instance_profile.name
+  }
+}
+
+resource "aws_autoscaling_group" "ghost_ec2_pool" {
+  name                = "${var.project}-ghost"
+  vpc_zone_identifier = [aws_subnet.public[0].id, aws_subnet.public[1].id, aws_subnet.public[2].id] ## Subnets hardcoded
+  max_size            = 1
+  min_size            = 1
+  target_group_arns   = [aws_lb_target_group.ghost-ec2.id]
+  launch_template {
+    id      = aws_launch_template.ghost-lt.id
+    version = "$Latest"
   }
 }
