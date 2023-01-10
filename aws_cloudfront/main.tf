7e6ec0c0d0f33b986cad5b0ede3a13bc90cf10a2
Yaroslav Kovalevskyi <kovalevskyi96@gmail.com>
2023-01-10 13:17:54 +0200

    tags, json linter

--- aws_cloudfront/main.tf
+++ aws_cloudfront/main.tf
@@ -1,8 +1,15 @@
 provider "aws" {
   region = "us-east-1" # <<<
+
+  default_tags {
+    tags = {
+      terraform = "true"
+      Owner     = "YK"
+    }
+  }
 }
 
-#Need to be used if bucket is located not in us-east-1 region
+#Need to use if bucket is located not in us-east-1 region
 provider "aws" {
   region = "ap-northeast-3" # <<<
   alias  = "bucket_location"
@@ -10,8 +17,8 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "kovalevskyi-remote-tfstate" # <<<
+    bucket = "kovalevskyi-remote-tfstate"   # <<<
     key    = "cloudfront/terraform.tfstate" # <<<
-    region = "eu-central-1"  # <<<
+    region = "eu-central-1"                 # <<<
   }
 }
--- aws_cloudfront/outputs.tf
+++ aws_cloudfront/outputs.tf
@@ -2,6 +2,3 @@ output "CloudFront_URL" {
   value = aws_cloudfront_distribution.cf_from_tf.aliases
 }
 
-output "bucket_endpoint" {
-  value = data.aws_s3_bucket.yk-cf-from-tf.website_endpoint
-}
